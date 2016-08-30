94347ac5fca7fff7667eefb46ce9a022bac9d66b
geordyp <geowilliams6@gmail.com>
2016-08-29 23:12:35 -0500

    Add handler for "Save All" menu action

---
+++ src/edu/ksu/wildcat/handlers/SaveAll.java
@@ -0,0 +1,33 @@
+package edu.ksu.wildcat.handlers;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.commands.IHandler;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
+
+/**
+ * Handler for the "Save All" menu action which saves all the active files
+ *
+ * @author geordypaul
+ */
+public class SaveAll extends AbstractHandler implements IHandler {
+
+	private IWorkbenchPage page;
+
+	/**
+	 * Executes with the map of parameter values by name
+	 *
+	 * @param event - contains all the info about the current state of the app
+	 * @return - the result of the execution
+	 */
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();		
+		page.saveAllEditors(false);
+
+		return null;
+	}
+}
