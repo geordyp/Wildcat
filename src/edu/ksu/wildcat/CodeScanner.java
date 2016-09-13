b78d61f26a18f16f25bd7b722c710c726e3a2f54
geordyp <geowilliams6@gmail.com>
2016-09-13 10:51:42 -0500

    Create a dictionary for syntax highlighting
    Created a new dictionary with every keyword which gets
    plugged into CodeScanner.java for syntax highlighting.
    The dictionary currently doesn't include alias keywords.

--- src/edu/ksu/wildcat/CodeScanner.java
+++ src/edu/ksu/wildcat/CodeScanner.java
@@ -1,6 +1,10 @@
 package edu.ksu.wildcat;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import org.eclipse.jface.text.TextAttribute;
 import org.eclipse.jface.text.rules.EndOfLineRule;
@@ -20,8 +24,8 @@ import org.eclipse.jface.text.rules.WordRule;
  */
 public class CodeScanner extends RuleBasedScanner {
 	
-	private static String[] _mainKeywords = {"environment", "method", "model", "variables", "interface", "responses"};
-	private static String[] _regularKeywords = {"tabular_graphics_data", "vector_parameter_study", "final_point", "num_steps", "single", "continuous_design", "initial_point", "descriptors", "system", "analysis_driver", "parameters_file", "results_file", "file_save", "num_objective_functions", "no_gradients", "no_hessians"};
+	private static String[] _mainKeywords; // = {"environment", "method", "model", "variables", "interface", "responses"};
+	private static String[] _regularKeywords; // = {"tabular_graphics_data", "vector_parameter_study", "final_point", "num_steps", "single", "continuous_design", "initial_point", "descriptors", "system", "analysis_driver", "parameters_file", "results_file", "file_save", "num_objective_functions", "no_gradients", "no_hessians"};
 	
 	/**
 	 * Constructor
@@ -53,6 +57,13 @@ public class CodeScanner extends RuleBasedScanner {
 		// Add generic whitespace rule
 		rules.add(new WhitespaceRule(new WhitespaceDetector()));
 
+		try {
+			getKeywords();			
+		}
+		catch(Exception e) {
+			// TODO
+		}
+		
 		// Add word rule for main keywords
 		WordRule wordRule = new WordRule(new WordDetector(), other);
 		for (int i = 0; i < _mainKeywords.length; i++)
@@ -64,6 +75,34 @@ public class CodeScanner extends RuleBasedScanner {
 		IRule[] result = new IRule[rules.size()];
 		rules.toArray(result);
 		setRules(result);
+	}
+	
+	private static void getKeywords() throws IOException {
+		List<String> mainKeywords = new ArrayList<String>();
+		List<String> regularKeywords = new ArrayList<String>();
+
+		try {
+			Scanner s = new Scanner(new File("C:/Users/geordypaul/Documents/Research/Wildcat/edu.ksu.wildcat/utility/dakota6.input.dictionary"));
+			while (s.hasNext()) {
+				String line = s.nextLine();
+				// remove "TAG "
+				line = line.substring(5);
+				String[] words = line.split("/");
+				if (words.length == 1) {
+					mainKeywords.add(words[0]);
+				}
+				else {
+					regularKeywords.add(words[words.length - 1]);					
+				}
+			}
+			
+			_regularKeywords = regularKeywords.toArray(new String[1]);
+			_mainKeywords = mainKeywords.toArray(new String[1]);
+			s.close();
+		}
+		catch(Exception e) {
+			// TODO
+		}
 	}
 
 }
